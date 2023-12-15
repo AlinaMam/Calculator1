daf13756b9c92814979d48e642356fa9345f612c
Alina <alinaamamontova@gmail.com>
2023-12-15 19:49:06 +0300

    Initial commit

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,70 @@
+package org.example;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Scanner;
+
+public class Calculator {
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final Logger LOGGER = LogManager.getLogger(Calculator.class);
+    public static void main(String[] args) {
+        int exit = 1;
+        while (exit != 0) {
+            int num1;
+            int num2;
+            try {
+                System.out.print("Введите число1: ");
+                num1 = SCANNER.nextInt();
+                LOGGER.info("Введено число1: " + num1);
+                System.out.println("Выберите операцию: " +
+                        "1. + " +
+                        "2. - " +
+                        "3. * " +
+                        "4. / ");
+                String line = SCANNER.next();
+                LOGGER.info("Введена операция: " + line);
+                System.out.print("Введите число2: ");
+                num2 = SCANNER.nextInt();
+                LOGGER.info("Введено число2: " + num2);
+                if (num2 == 0 && line.equals("/")) {
+                    System.out.println("На 0 делить нельзя");
+                    LOGGER.error("Деление на 0");
+                    break;
+                }
+                getResult(num1, num2, line, SCANNER);
+                System.out.print("Если хочешь продолжить введи 1. Для выхода введи 0: ");
+                exit = SCANNER.nextInt();
+                LOGGER.info("Выбран: " + exit);
+            } catch (Exception e) {
+                LOGGER.error("Пользователь ввел некорректное число");
+                break;
+            }
+        }
+    }
+
+    public static void getResult(int num1, int num2, String line, Scanner scanner) {
+        double result = 0.0;
+        switch (line) {
+            case "+":
+                System.out.println(num1 + " + " + num2 + " = " + (result = num1 + num2));
+                LOGGER.info("Прошла операция сложения: " + num1 + " + " + num2 + " = " + (result = num1 + num2));
+                break;
+            case "-":
+                System.out.println(num1 + " - " + num2 + " = " + (result = num1 - num2));
+                LOGGER.info("Прошла операция вычитания: " + num1 + " - " + num2 + " = " + (result = num1 - num2));
+                break;
+            case "*":
+                System.out.println(num1 + " * " + num2 + " = " + (result = num1 * num2));
+                LOGGER.info("Прошла операция умножения: " + num1 + " * " + num2 + " = " + (result = num1 * num2));
+                break;
+            case "/":
+                System.out.println(num1 + " / " + num2 + " = " + (result = num1 / num2));
+                LOGGER.info("Прошла операция деления: " + num1 + " / " + num2 + " = " + (result = num1 / num2));
+                break;
+            default:
+                System.out.println("Операция некорретна. Попробуйте сначала!");
+                break;
+        }
+    }
+}
